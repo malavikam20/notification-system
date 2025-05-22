16788aca725604d90ff971da1c12f4754524b6dc
malavikam20 <malavikamanoj495@gmail.com>
2025-05-22 15:40:29 +0530

    Initial Commit

---
+++ api/server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const cors = require('cors');
+
+
+const app = express();
+const server = http.createServer(app);
+
+const io = socketIo(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST'],
+  },
+});
+
+app.use(cors());
+app.use(express.json());
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
+app.post('/send', (req, res) => {
+  const message = req.body.message;
+  console.log('Received message:', message);
+
+  io.emit('pushnotification', { message });
+  res.status(200).send({ message: 'Message sent' });
+});
+
+server.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
